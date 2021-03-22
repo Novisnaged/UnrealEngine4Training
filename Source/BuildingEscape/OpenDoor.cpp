6c98c1d930b0785dbf89aebf4397eef92720e36d
unknown <ata.abg@gmail.com>
2021-03-22 23:23:44 +0300

    Initial commit

---
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "OpenDoor.h"
+#include "GameFramework/Actor.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void UOpenDoor::OpenDoor()
+{
+	AActor* Owner = GetOwner();		//Find the owning actor
+
+	FRotator NewRotation = FRotator(0.00f, -60.00f, 0.00f);		//Create a rotator
+
+	Owner->SetActorRotation(NewRotation);		//Set the door rotation
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) 
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	//Poll the TriggerVolume
+	//If the ActorThatOpens is in the volume
+		if(PressurePlate->IsOverlappingActor(ActorThatOpens))
+			OpenDoor();
+}
